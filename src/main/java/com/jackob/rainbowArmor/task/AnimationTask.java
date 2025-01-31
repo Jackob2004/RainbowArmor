d2474142f7d0bca02a3699e49d7e5bdd5b6edb83
Jackob2004 <zjakub2004@gmail.com>
2025-01-31 19:21:25 +0100

    [Feature] create AnimationTask

--- src/main/java/com/jackob/rainbowArmor/task/AnimationTask.java
+++ src/main/java/com/jackob/rainbowArmor/task/AnimationTask.java
@@ -1,4 +1,39 @@
 package com.jackob.rainbowArmor.task;
 
-public class AnimationTask {
+import org.bukkit.Color;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Random;
+
+public class AnimationTask extends BukkitRunnable {
+
+    private ItemStack[] armorPieces;
+
+    public AnimationTask(ItemStack[] armorPieces) {
+        this.armorPieces = armorPieces;
+    }
+
+    @Override
+    public void run() {
+        changeArmorColor();
+    }
+
+    private void changeArmorColor() {
+        Color color = randomColor();
+
+        for (ItemStack item : armorPieces) {
+            LeatherArmorMeta armorMeta = (LeatherArmorMeta) item.getItemMeta();
+
+            armorMeta.setColor(color);
+        }
+    }
+
+    private Color randomColor() {
+        Random random = new Random();
+
+        return Color.fromRGB(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+    }
+
 }
