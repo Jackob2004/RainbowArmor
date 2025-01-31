a2eb9c3328afa3202f398b50e5b69c4047e138a8
Jackob2004 <zjakub2004@gmail.com>
2025-01-31 18:43:38 +0100

    [Feature] add leather armor equip functionality
    Add createLeatherArmor(), hasNoArmor(), and equipLeatherArmor() methods, messageIndicator variable

--- src/main/java/com/jackob/rainbowArmor/manager/RainbowManager.java
+++ src/main/java/com/jackob/rainbowArmor/manager/RainbowManager.java
@@ -1,14 +1,54 @@
 package com.jackob.rainbowArmor.manager;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Color;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
 
 public class RainbowManager {
 
-    public void activateRainbow(Player player) {
+    private final String messageIndicator = "[Rainbow Armor] ";
 
+    public void activateRainbow(Player player) {
+        equipLeatherArmor(player);
+    }
+
+    private ItemStack[] equipLeatherArmor(Player player) {
+        ItemStack[] leatherArmor = createLeatherArmor();
+        
+        if (hasNoArmor(player)) {
+            player.getInventory().setArmorContents(leatherArmor);
+            
+            return leatherArmor;
+        } else {
+            player.sendMessage(messageIndicator + ChatColor.GRAY + "You need to remove your current armor");
+        }
+        
+        return null;
+    }
+
+    private boolean hasNoArmor(Player player) {
+        for (ItemStack item : player.getInventory().getArmorContents()) {
+            if (item != null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private ItemStack[] createLeatherArmor() {
+        ItemStack[] armor = new ItemStack[4];
+
+        armor[0] = new ItemStack(Material.LEATHER_BOOTS);
+        armor[1] = new ItemStack(Material.LEATHER_LEGGINGS);
+        armor[2] = new ItemStack(Material.LEATHER_CHESTPLATE);
+        armor[3] = new ItemStack(Material.LEATHER_HELMET);
+
+        return armor;
     }
 
     private Color randomColor() {
